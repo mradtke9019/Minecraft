284f135b70b7ab59c3602899d1a07176ee103ace
Matt Radtke <more>
2023-03-15 14:01:46 +0000

    Add translateable and transformable classes to contribute to the modeling transform hierarchy

---
+++ Minecraft/Minecraft/ITranslatable.h
@@ -0,0 +1,29 @@
+
+#pragma once
+#include <glm/fwd.hpp>
+
+class ITranslatable {
+private:
+	glm::vec3 Position;
+
+public:
+	ITranslatable()
+	{
+		Position = glm::vec3(0, 0, 0);
+	}
+
+	glm::mat4 GetTranslationMatrix()
+	{
+		return glm::translate(glm::mat4(1), Position);
+	}
+
+	glm::vec3 GetPosition()
+	{
+		return Position;
+	}
+
+	void SetPosition(glm::vec3 p)
+	{
+		Position = p;
+	}
+};
--- Minecraft/Minecraft/Model.h
+++ Minecraft/Minecraft/Model.h
@@ -1,5 +1,8 @@
+#include "ITransformable.h"
+
 #pragma once
-class Model
+class Model : public ITransformable
 {
+
 };
 
