425cda2127d737210d940690774193c229222cc1
Matt Radtke <more>
2023-03-20 21:51:29 +0000

    Camera changes

--- Minecraft/Minecraft/FixedCamera.h
+++ Minecraft/Minecraft/FixedCamera.h
@@ -4,7 +4,7 @@
 class FixedCamera : public ICamera
 {
 private:
-	glm::vec3 Position;
+
 	glm::vec3 Target;
 	glm::vec3 Up;
 
@@ -17,17 +17,7 @@ public:
 
 	glm::mat4* GetViewTransform();
 
-	glm::vec3 GetPosition();
-
-	void SetPosition(glm::vec3);
-
 	glm::vec3 GetTarget();
 
 	void SetTarget(glm::vec3);
-
-	// Dont implement these for a fixed camera
-	void TranslateX(float x) {};
-	void TranslateY(float x) {};
-	void TranslateZ(float x) {};
-	void RotateYaw(float x) {};
 };
